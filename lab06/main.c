e5a61219986f590fe33e4ed8a44e83978de1e815
IIYuZoII <mystudymailms@gmail.com>
2021-12-10 01:24:27 +0200

    Save #2

--- lab06/main.c
+++ lab06/main.c
@@ -13,11 +13,11 @@
 
 int main (){
 
-	int x = 9;
+	int x = 2;
 
 	int n_in_m1 = 0;
         char array_num_words[]="one,two,three,four,five,six,seven,eight,nine\0";
-        char m2[]="";
+        //char *m2;
 
 
 
@@ -25,11 +25,12 @@ int main (){
         printf ("%s\n", array_num_words);
 
 	//Переменные букв и слов, переменная букв обнуляется при каждой запятой или символе конца строки
+	int temp_count_letters = 0;
 	int count_letters = 0;
 	int count_words = 0;
 
 	
-        
+        char m2[count_letters];
 
 	//Перебор чара, увеличение счётчика букв и его последующее обнуление при появление не буквенного символа и увеличение кол-ва слов
 	for (int i = 0; i < 60; i++){
@@ -44,13 +45,14 @@ int main (){
 
 		//Увеличение счётчиа кол-ва букв и его обнуление при достижении запятой или конца строки (для того чтобы начать записывать кол-во букв след. слова) а так же счётчик слов
 		if (array_num_words[i] != ',' && array_num_words[i] != '\0'){
-			count_letters++;
+			temp_count_letters++;
 		} else {
 			count_words++;
-			count_letters = 0;
+			count_letters = temp_count_letters;
+			temp_count_letters = 0;
 		}
 
-		if (count_words == x - 1){
+		if (count_words == x - 1 && array_num_words[i] != ','){
 			printf ("%c\n", array_num_words[i]);
 			m2[i] = array_num_words[i];
 
@@ -68,7 +70,7 @@ int main (){
 
 
 
-	
+	printf ("%s\n", m2);
 	printf ("%d", count_words);
 	printf ("\n");
 
