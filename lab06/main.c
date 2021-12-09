402a95f6e6c405cb9297b4b7fd1c196bd878c1bd
IIYuZoII <mystudymailms@gmail.com>
2021-12-10 01:08:01 +0200

    Save #1

---
+++ lab06/main.c
@@ -0,0 +1,78 @@
+/* Перетворити число (максимальне значення якого - 9999) в рядок. (усі символи
+ * нижнього регістру на виході). Наприклад,
+ * • 123 – “one hundred twenty three”,
+ * • 4311 – “four thousands three hundreds eleven”
+ *
+ *
+ *
+ *
+ */
+
+#include <stdio.h>
+
+
+int main (){
+
+	int x = 9;
+
+	int n_in_m1 = 0;
+        char array_num_words[]="one,two,three,four,five,six,seven,eight,nine\0";
+        char m2[]="";
+
+
+
+	//Вывод всего массива чисел в виде слов
+        printf ("%s\n", array_num_words);
+
+	//Переменные букв и слов, переменная букв обнуляется при каждой запятой или символе конца строки
+	int count_letters = 0;
+	int count_words = 0;
+
+	
+        
+
+	//Перебор чара, увеличение счётчика букв и его последующее обнуление при появление не буквенного символа и увеличение кол-ва слов
+	for (int i = 0; i < 60; i++){
+
+		//Перебор числа и его вывод в консоль (игнорируя запятые)
+		/*if (array_num_words[i] != ','){
+			printf ("%c\n", array_num_words[i]);
+		} else {
+			printf ("\n\n");
+		}*/
+		
+
+		//Увеличение счётчиа кол-ва букв и его обнуление при достижении запятой или конца строки (для того чтобы начать записывать кол-во букв след. слова) а так же счётчик слов
+		if (array_num_words[i] != ',' && array_num_words[i] != '\0'){
+			count_letters++;
+		} else {
+			count_words++;
+			count_letters = 0;
+		}
+
+		if (count_words == x - 1){
+			printf ("%c\n", array_num_words[i]);
+			m2[i] = array_num_words[i];
+
+		}		
+
+
+		//Проверка конца строки и остановка цикла при её достижении
+		if (array_num_words[i] == '\0'){
+			break;
+		}	
+
+	
+	
+	}
+
+
+
+	
+	printf ("%d", count_words);
+	printf ("\n");
+
+        return (0);
+
+
+}
